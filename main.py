ee7f2c5540bb7368f05ad9f3370f6a40689e9d7b
tol2000 <tolicheg@gmail.com>
2020-09-10 17:08:56 +0300

    :)

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ if __name__ == '__main__':
     print_hi('Tolic')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+array = [1,2,3,4,5,6,7]
+
+looping = True
+
+while len(array) > 0:
+    popa = array.pop(0)
+    if popa != None:
+        print(popa)
+    else:
+        looping = False
+
