0de7bd673b9dfbc83c8263e0f58d0cd9e190c913
tol2000 <tolicheg@gmail.com>
2020-09-11 08:59:02 +0300

    main module finished :)

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
-# This is a sample Python script.
-
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hail, {name}!')  # Press Ctrl+8 to toggle the breakpoint.
+    print(f'{__name__}: Hail, {name}!')  # Press Ctrl+8 to toggle the breakpoint.
 
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Tolic')
+print_hi('Tolic')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
+
 array = [1,2,3,4,5,6,7]
 
-looping = True
-
 while len(array) > 0:
-    popa = array.pop(0)
-    if popa != None:
-        print(popa)
-    else:
-        looping = False
-
+    pop_var = array.pop(0)
+    print(pop_var)
