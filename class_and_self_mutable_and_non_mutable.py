45c51d2531cd1feb55e216843d2cfab1ba015a17
tol2000 <tolicheg@gmail.com>
2021-04-20 16:59:30 +0300

    class_and_self_mutable_and_non_mutable.py

---
+++ class_and_self_mutable_and_non_mutable.py
@@ -0,0 +1,16 @@
+class Test:
+    instances = []
+    x = 0
+
+    def __init__(self):
+        self.instances.append(id(self))
+        self.x += 1
+        Test.x += 1
+        print(f'self.instances = {self.instances}')
+        print(f'self.x = {self.x}')
+        print(f'Test.x = {Test.x}')
+
+
+a = Test()
+b = Test()
+c = Test()
