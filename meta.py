c21b8248075cd7e84c69ade12769f3901de0c5f4
tol2000 <tolicheg@gmail.com>
2021-10-22 13:25:06 +0300

    lesson 5

--- meta.py
+++ meta.py
@@ -1,3 +1,44 @@
-class MyMetaClass:
-    def __new__(cls, name, bases, dct, *args, **kwargs):
-        return super().__new__(cls, name, bases, dct, *args, **kwargs)
+def dict_wo_dunder(dct: dict):
+    dct1 = {}
+    for key in dct.keys():
+        if not (key.startswith("__") and key.endswith("__")):
+            dct1[key] = dct[key]
+    return dct1
+
+
+def print_namespaces_with_parents(cls):
+    for parent in cls.__bases__:
+        if parent.__name__ != 'object':
+            print_namespaces_with_parents(parent)
+    print(f"{cls.__name__}: {dict_wo_dunder(cls.__dict__)}")
+
+
+
+class MyMetaClass(type):
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        print("New class", name)
+        print("Bases", bases)
+        print("Namespace", dict_wo_dunder(namespace))
+        return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+
+class MyMetaClass1(MyMetaClass):
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        print("New 1")
+        return super().__new__(mcs, name, bases, namespace, **kwargs)
+
+
+NewFoo = MyMetaClass('NewFoo', (), {"spam": "eggs"})
+
+
+class NewFoo1(NewFoo):
+    spam1 = 'eggs1'
+
+
+class NewFoo2(NewFoo1, metaclass=MyMetaClass1):
+    foo = 'bar'
+    ba = 'for'
+
+
+print("-------------------------------")
+print_namespaces_with_parents(NewFoo2)
