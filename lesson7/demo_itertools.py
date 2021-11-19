c5735c28cc1f7b821e461de9cc0e32a7256818e4
tol2000 <tolicheg@gmail.com>
2021-11-19 18:02:30 +0200

    deleted venv from repo

--- lesson7/demo_itertools.py
+++ lesson7/demo_itertools.py
@@ -1,8 +1,21 @@
-from itertools import combinations
+from itertools import combinations, combinations_with_replacement,\
+    count, permutations
 
-max_len = 2
+max_len = 3
 
-nums = [0, 1, 2, 3, 4, 5]
+nums = range(10)
 
-for comb in combinations(nums, max_len):
-    print(comb)
+print(nums)
+
+# for comb in enumerate(combinations(nums, max_len)):
+#     print(f'{comb[0]}:  {comb[1]}')
+#
+# for comb in combinations_with_replacement(nums, max_len):
+#     print(comb)
+
+for comb in enumerate(permutations('ABCD', 2)):
+    print(f'{comb[0]}:  {comb[1]}')
+
+odds = count(start=1, step=2)
+for _ in range(10):
+    print(next(odds))
