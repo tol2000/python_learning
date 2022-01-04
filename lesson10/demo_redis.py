2ec28943670efeff6a6d9ce6d66c18caf6b6a945
tol2000 <tolicheg@gmail.com>
2022-01-04 15:17:06 +0200

    lesson 10: some experiments from me :)

--- lesson10/demo_redis.py
+++ lesson10/demo_redis.py
@@ -5,17 +5,20 @@ USERS_FUNDS = {}
 
 r = redis.Redis()
 
-print("foo:", r.get("foo"))
-
-print("spam:", r.get("spam"))
-
-print("A, B, D:", r.mget(("A", "B", "D")))
-print(r.set("qwe", 123))
-print(r.get("qwe"))
-
-print(r.setex("qwerty", 1, "foobar"))
-print(r.get("qwerty"))
-sleep(1)
-print(r.get("qwerty"))
-
-print(r.hgetall("lessons"))
+print("mainnumber:", str(r.get('mainnumber'), 'UTF-8'))
+r.set('newnumber', 'пятьдесят шесть')
+print('newnumber', str(r.get('newnumber'), 'UTF-8'))
+# print("foo:", r.get("foo"))
+#
+# print("spam:", r.get("spam"))
+#
+# print("A, B, D:", r.mget(("A", "B", "D")))
+# print(r.set("qwe", 123))
+# print(r.get("qwe"))
+#
+# print(r.setex("qwerty", 1, "foobar"))
+# print(r.get("qwerty"))
+# sleep(1)
+# print(r.get("qwerty"))
+#
+# print(r.hgetall("lessons"))
