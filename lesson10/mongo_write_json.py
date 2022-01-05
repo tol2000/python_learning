a6faf9ec84b61f158338f6a5644566ad56aafcd2
tol2000 <tolicheg@gmail.com>
2022-01-05 18:20:25 +0200

    lesson 10: mongo

--- lesson10/mongo_write_json.py
+++ lesson10/mongo_write_json.py
@@ -1,17 +1,20 @@
 import json
-from pprint import pprint
-
 import pymongo
 
 """
 in mongosh: db.json_table.find({text:{$regex:'смарт',$options:'i'}})
 """
 
-json_file_name = '/home/tolic/gitrepo/learn_and_try/stuff/site_parser/articles.json'
+# json_file_name = '/home/tolic/gitrepo/learn_and_try/stuff/site_parser/articles.json'
+# json_file_name = '/home/tolic/gitrepo/github/my_python_projects/learn_and_try/' \
+#                  'stuff/site_parser/done/articles_from_hitech.json'
+
+json_file_name = '/home/tolic/gitrepo/github/my_python_projects/' \
+                 'learn_and_try/stuff/site_parser/articles.json'
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-db = client['my_json_db']
+db = client['json_db']
 json_table = db['json_table']
 json_table.drop()
 
