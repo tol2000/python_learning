617bf5ccf5d1702cf5f1eb1a02115cc46549abe2
tol2000 <tolicheg@gmail.com>
2022-01-07 19:53:35 +0200

    lesson 10: postgres end

--- lesson10/demo_pg.py
+++ lesson10/demo_pg.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from pprint import pprint
+
 import psycopg2
 
 
@@ -15,12 +18,13 @@ cur = conn.cursor()
 print(cur)
 
 res = cur.execute("SELECT id, full_name, username FROM users;")
-print(res)
+pprint(res)
 
 users = cur.fetchall()
-print(users)
+pprint(users)
 
-res = cur.execute("INSERT INTO users (username, full_name) VALUES ('sam', 'Sam White');")
+dt = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
+res = cur.execute(f"INSERT INTO users (username, full_name) VALUES ('sam_{dt}', 'Sam was born in {dt}');")
 print(conn.commit())
 
 
