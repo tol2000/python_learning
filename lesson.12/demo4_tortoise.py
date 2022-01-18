ddc98d492d79bf2cd0541ee5fd72e8254d90d0d5
tol2000 <tolicheg@gmail.com>
2022-01-18 22:53:08 +0200

    lesson 12: demo4_tortoise.py

--- lesson.12/demo4_tortoise.py
+++ lesson.12/demo4_tortoise.py
@@ -6,13 +6,18 @@ class Tournament(Model):
     id = fields.IntField(pk=True)
     name = fields.TextField()
 
+    def __str__(self):
+        attrs = ['id', 'name']
+        attrs_str = ', '.join([f'{k}={self.__getattribute__(k)}' for k in attrs])
+        return f'{self.__class__.__name__}({attrs_str})'
+
 
 async def init():
     # Here we create a SQLite DB using file "db.sqlite3"
     #  also specify the app name of "models"
     #  which contain models from "app.models"
     await Tortoise.init(
-        db_url="postgres://username:password@localhost/demo",
+        db_url="postgres://user:password@localhost/postgres",
         modules={'models': ['__main__']}
     )
     # Generate the schema
@@ -22,15 +27,23 @@ async def init():
 async def main():
     await init()
 
+    await Tournament.all().delete()
+
     tournament = Tournament(name='New Tournament')
     await tournament.save()
 
     # Or by .create()
     await Tournament.create(name='Another Tournament')
+    await Tournament.create(name='Tournament Толяна')
+    await Tournament.create(name='Tournament Коляна')
+    await Tournament.create(name='Tournament всех турнаментов')
 
     # Now search for a record
-    tour = await Tournament.filter(name__contains='Another').first()
-    print(tour.id, tour.name)
+    for tour in await Tournament.filter(name__contains='оляна').all():
+        print(tour.id, tour.name)
+
+    for t in await Tournament.all():
+        print(t)
 
 
 if __name__ == "__main__":
