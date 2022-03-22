cc6fb1ca67586d0a5677ad9345fa86bd829f9fbd
tol2000 <tolicheg@gmail.com>
2022-03-22 23:06:51 +0200

    lesson 17: process

--- lesson.17/tface.py
+++ lesson.17/tface.py
@@ -1,16 +1,52 @@
 import os
 # from pathlib import Path
 import cv2
+import numpy as np
 
 FRONTAL_FACE = "haarcascade_frontalface_default.xml"
+# PROFILE_FACE = "haarcascade_profileface_default.xml"
 PATH = '/media/sf_VBOX_PUBLIC/temp/Photos/'
+
+faces_cascade_db = cv2.CascadeClassifier(cv2.data.haarcascades + FRONTAL_FACE)
+
+
+def show_img(img):
+    win_name = 'Image'
+    cv2.namedWindow(win_name, flags=cv2.WINDOW_NORMAL)
+    cv2.moveWindow(win_name, 1, 1)
+    cv2.resizeWindow(win_name, 1800, 800)
+    cv2.imshow(win_name, img)
+    cv2.waitKey()
+
+
+def main2():
+
+    # profile
+    # p = '/media/sf_VBOX_PUBLIC/temp/Photos/DSLR/Репортаж/События/ДР_Одессы_2008/2008-09-03_01-10__DSC0042.jpg'
+
+    # frontal
+    # p = '/media/sf_VBOX_PUBLIC/temp/Photos/DSLR/Репортаж/На_работе/НГ_2006//D01_9139.jpg'
+    p = '/media/sf_VBOX_PUBLIC/temp/Photos/DSLR/Репортаж/На_работе/НГ_2007_2008/D200805_3250_002.jpg'
+
+    img = cv2.imread(p)
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = faces_cascade_db.detectMultiScale(img)
+    for i, (x, y, w, h) in enumerate(faces):
+        pt1 = (x, y)
+        pt2 = (x + w, y + h)
+        b = (i * 10 + int(x) * i) % 255
+        g = (i * 25 + int(y) * i) % 255
+        color = (b, g, 255)
+        thickness = 2
+        cv2.rectangle(img, pt1, pt2, color, thickness)
+    show_img(img)
 
 
 def main1():
-    p = '/media/sf_VBOX_PUBLIC/temp/Photos/DSLR/Репортаж/События/Бодибилдинг_Одесса_апрель_2007/D200708_3917.jpg'
-    img = cv2.imread(p)
-    cv2.imshow('Image', img)
-    cv2.waitKey()
+    shape = (480, 640, 3)
+    img = np.zeros(shape, np.uint8)
+    img[...] = [170, 255, 255]
+    show_img(img)
 
 
 def main():
@@ -26,4 +62,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main1()
+    main2()
+    cv2.destroyAllWindows()
