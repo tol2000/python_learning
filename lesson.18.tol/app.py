1138b07c78558334e74e916b7df6548e72b3edb3
tol2000 <tolicheg@gmail.com>
2022-04-03 02:35:16 +0300

    REL 0.51: lesson 18: tol: tags h1 for phone

--- lesson.18.tol/app.py
+++ lesson.18.tol/app.py
@@ -5,6 +5,10 @@ import base64
 import io
 from pathlib import Path
 
+TAG_DIR_NAME = 'H1'
+TAG_DIR_ROOT_UP = 'H1'
+TAG_DIR_FILES_LIST = 'H1'
+
 SUBDIR_PARAM_NAME = 'subdir'
 PICTURE_PATH_PARAM_NAME = 'picture'
 DIR_ENDPOINT = 'dir'
@@ -48,7 +52,8 @@ def show_picture():
     return render_template(
         'picture.html',
         img_data=encoded_img_data.decode('utf-8'),
-        img_subdir_link=make_url_for_subdir(request.url_root + f'{DIR_ENDPOINT}/', picture_dir_path)
+        img_subdir_link=make_url_for_subdir(request.url_root + f'{DIR_ENDPOINT}/', picture_dir_path),
+        tag_dir_root_up=TAG_DIR_ROOT_UP
     )
 
 
@@ -64,18 +69,18 @@ def show_dir():
         dir_name = app_work_dir
         subdir = ''
 
-    out_text = f'<h3>Directory: {subdir if subdir else "root"}</h3><BR>'
+    out_text = f'<{TAG_DIR_NAME}>Directory: {subdir if subdir else "root"}</{TAG_DIR_NAME}><BR>'
     # out_text += f'\n<BR>app_work_dir: {app_work_dir}\n<BR>dir_name: {dir_name}'
 
     # root dir
     url = make_url_for_subdir(dir_url, '')
-    out_text += f'\n<h4><a href="{url}">Root directory</a></h4>'
+    out_text += f'\n<{TAG_DIR_ROOT_UP}><a href="{url}">Root directory</a></{TAG_DIR_ROOT_UP}>'
 
     # Parent dir (..)
     if app_work_dir in dir_name.parents:
         subdir_to_up = str(dir_name.parent)[len(str(app_work_dir))+1:]
         url = make_url_for_subdir(dir_url, subdir_to_up)
-        out_text += f'\n<h4><a href="{url}">.. (up dir)</a></h4>'
+        out_text += f'\n<{TAG_DIR_ROOT_UP}><a href="{url}">.. (up dir)</a></{TAG_DIR_ROOT_UP}>'
 
     dir_list = sorted(list(dir_name.iterdir()), key=lambda x: (not x.is_dir(), str(x)))
     for path_obj in dir_list:
@@ -83,9 +88,9 @@ def show_dir():
         path_for_display = path_obj.relative_to(dir_name)
         if path_obj.is_dir():
             url = make_url_for_subdir(dir_url, path_for_url)
-            out_text += f'\n<h4><a href="{url}">{path_for_display}</a></h4>'
+            out_text += f'\n<{TAG_DIR_FILES_LIST}><a href="{url}">{path_for_display}</a></{TAG_DIR_FILES_LIST}>'
         elif path_obj.suffix.lower() in ['.jpg', '.jpeg']:
             url = make_picture_url_for_subdir(picture_url, Path(subdir), Path(path_for_display))
-            out_text += f'\n<h4><a href="{url}">{path_for_display}</a></h4>'
+            out_text += f'\n<{TAG_DIR_FILES_LIST}><a href="{url}">{path_for_display}</a></{TAG_DIR_FILES_LIST}>'
 
     return out_text
