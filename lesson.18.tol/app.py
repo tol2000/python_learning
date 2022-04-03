82ce028740df5957780eb0db93645361323274da
tol2000 <tolicheg@gmail.com>
2022-04-03 18:44:57 +0300

    REL 0.6: lesson 18: tol: dir template and zoom parameter

--- lesson.18.tol/app.py
+++ lesson.18.tol/app.py
@@ -5,29 +5,15 @@ import base64
 import io
 from pathlib import Path
 
-TAG_DIR_NAME = 'H1'
-TAG_DIR_ROOT_UP = 'H1'
-TAG_DIR_FILES_LIST = 'H1'
-
 SUBDIR_PARAM_NAME = 'subdir'
 PICTURE_PATH_PARAM_NAME = 'picture'
 DIR_ENDPOINT = 'dir'
 PICTURE_ENDPOINT = 'picture'
 
-app_dir = Path(__file__).resolve().absolute().parent
-app_work_dir = (app_dir / Path('public_files')).resolve().absolute()
+app_dir: Path = Path(__file__).resolve().absolute().parent
+app_work_dir: Path = (app_dir / Path('public_files')).resolve().absolute()
 
 app = Flask('photo_v')
-
-
-@app.route('/')
-def index():
-    return '<h1>Hello from Tolyan (Flask)!</h1>'
-
-
-@app.route('/test/', methods=['HEAD'])
-def test():
-    return 'Bebebe'
 
 
 def make_url_for_subdir(dir_url, path_for_url):
@@ -42,7 +28,8 @@ def make_picture_url_for_subdir(picture_url: str, picture_subdir: Path, picture_
 
 
 @app.route(f'/{PICTURE_ENDPOINT}/')
-def show_picture():
+@app.route(f'/{PICTURE_ENDPOINT}/<int:zoom>/')
+def show_picture(zoom=4):
     picture_path = request.args.get(f'{PICTURE_PATH_PARAM_NAME}', '', str)
     picture_dir_path = request.args.get(f'{SUBDIR_PARAM_NAME}', '', str)
     im = Image.open(app_work_dir / Path(picture_path))
@@ -52,45 +39,55 @@ def show_picture():
     return render_template(
         'picture.html',
         img_data=encoded_img_data.decode('utf-8'),
-        img_subdir_link=make_url_for_subdir(request.url_root + f'{DIR_ENDPOINT}/', picture_dir_path),
-        tag_dir_root_up=TAG_DIR_ROOT_UP
+        img_subdir_link=make_url_for_subdir(request.url_root + f'{DIR_ENDPOINT}/{zoom}/', picture_dir_path),
+        zoom=zoom,
     )
 
 
 @app.route(f'/{DIR_ENDPOINT}/')
-def show_dir():
+@app.route(f'/{DIR_ENDPOINT}/<int:zoom>/')
+def show_dir(zoom=4):
+    """
+    :param zoom: 1, 2... - this number takes a part in <hn> tag, where n is zoom_h_num
+                       for example, if you set 2 then all items will be displayed with <h2></h2> tags
+    :return:
+    """
     subdir = request.args.get(f'{SUBDIR_PARAM_NAME}', '', str)
     dir_name = (app_work_dir / subdir).resolve().absolute()
-    dir_url = request.url_root + f'{DIR_ENDPOINT}/'
-    picture_url = request.url_root + f'{PICTURE_ENDPOINT}/'
+    dir_url = request.url_root + f'{DIR_ENDPOINT}/{zoom}/'
+    picture_url = request.url_root + f'{PICTURE_ENDPOINT}/{zoom}/'
 
     # for security reasons :)
     if not str(dir_name).startswith(str(app_work_dir)):
         dir_name = app_work_dir
         subdir = ''
 
-    out_text = f'<{TAG_DIR_NAME}>Directory: {subdir if subdir else "root"}</{TAG_DIR_NAME}><BR>'
-    # out_text += f'\n<BR>app_work_dir: {app_work_dir}\n<BR>dir_name: {dir_name}'
-
-    # root dir
-    url = make_url_for_subdir(dir_url, '')
-    out_text += f'\n<{TAG_DIR_ROOT_UP}><a href="{url}">Root directory</a></{TAG_DIR_ROOT_UP}>'
-
     # Parent dir (..)
     if app_work_dir in dir_name.parents:
         subdir_to_up = str(dir_name.parent)[len(str(app_work_dir))+1:]
-        url = make_url_for_subdir(dir_url, subdir_to_up)
-        out_text += f'\n<{TAG_DIR_ROOT_UP}><a href="{url}">.. (up dir)</a></{TAG_DIR_ROOT_UP}>'
+    else:
+        subdir_to_up = ''
+    up_dir_link = make_url_for_subdir(dir_url, subdir_to_up)
 
     dir_list = sorted(list(dir_name.iterdir()), key=lambda x: (not x.is_dir(), str(x)))
+    dir_items = []
     for path_obj in dir_list:
         path_for_url = path_obj.relative_to(app_work_dir)
         path_for_display = path_obj.relative_to(dir_name)
         if path_obj.is_dir():
             url = make_url_for_subdir(dir_url, path_for_url)
-            out_text += f'\n<{TAG_DIR_FILES_LIST}><a href="{url}">{path_for_display}</a></{TAG_DIR_FILES_LIST}>'
         elif path_obj.suffix.lower() in ['.jpg', '.jpeg']:
             url = make_picture_url_for_subdir(picture_url, Path(subdir), Path(path_for_display))
-            out_text += f'\n<{TAG_DIR_FILES_LIST}><a href="{url}">{path_for_display}</a></{TAG_DIR_FILES_LIST}>'
+        else:
+            url = None
+        if url:
+            dir_items.append((path_for_display, url))
 
-    return out_text
+    return render_template(
+        'dir.html',
+        dir_path=subdir if subdir else "root",
+        root_dir_link=make_url_for_subdir(dir_url, ''),
+        up_dir_link=up_dir_link,
+        dir_items=dir_items,
+        zoom=zoom,
+    )
